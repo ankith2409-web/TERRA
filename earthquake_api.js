22f45d65762d05b73f07e1ae557c6838c7ae1434
ankith2409-web <kanchikumarswamy@gmail.com>
2026-05-16 21:51:36 +0530

    added the apiearthquake_api.js

---
+++ earthquake_api.js
@@ -0,0 +1,9 @@
+// The United States Geological Survey (USGS) provides a free, public API 
+
+const fetchLiveEarthquakes = async () => {
+    // You can change the endpoint based on your needs. Other options include:
+    // - all_day.geojson (All earthquakes past day)
+    // - significant_month.geojson (Significant earthquakes past month)
+    // - 4.5_day.geojson (Magnitude 4.5+ past day)
+    const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
+
