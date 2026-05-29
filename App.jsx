1bd377cc93be13729fd7fdb18da0612904a31e74
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-05-29 16:43:09 +0530

    added components

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import Dashboard from './components/Dashboard';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
+import StatsPanel from './components/StatsPanel';
+import Charts from './components/Charts';
+import Filters from './components/Filters';
+import SearchBar from './components/SearchBar';
+import LoadingSpinner from './components/LoadingSpinner';
