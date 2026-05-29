5afc989cfd15356d288bdee0e983d83c281c99cb
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-05-30 01:16:16 +0530

    dash content wrapper added

--- dashboard.jsx
+++ dashboard.jsx
@@ -23,3 +23,8 @@ const Dashboard = ({
       <div className="flex-1 flex flex-col min-w-0 transition-all duration-300">
         {/* Navbar */}
         {navbar}
+
+          {/* Dash Content Wrapper */}
+        <main className="p-4 md:p-6 lg:p-8 space-y-6 overflow-y-auto max-w-[1600px] mx-auto w-full">
+          {isLoading ? (
+            <div className="flex items-center justify-center min-h-[50vh]">
