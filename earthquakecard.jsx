e3594e1fa0b0b2e04bd260a32473de292e9cdbe1
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-03 12:27:40 +0530

    import react

---
+++ earthquakecard.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { MapPin, Compass, Clock, AlertTriangle } from 'lucide-react';
+import { getSeverityColor } from '../data/mockData';
+import '../styles/cards.css';
