9435018da9a989a4e73dbe8caa5c3a73b9fadba0
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-03 12:47:02 +0530

    import react

---
+++ loadingSpinner.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+const LoadingSpinner = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[60vh] text-cyan-400 font-mono">
+      <div className="relative w-24 h-24 mb-6">
